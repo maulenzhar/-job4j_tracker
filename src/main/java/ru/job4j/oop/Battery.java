f1e38595124a21c0ae6cc2c7e167f5eb5ed310f3
mzharylgasynov <$dbx254WSX>
2022-04-18 18:17:07 +0600

    1.8. Состояние объекта [#167388 #289980]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,8 +8,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = this.load;
+        another.load += this.load;
         this.load = 0;
     }
 
