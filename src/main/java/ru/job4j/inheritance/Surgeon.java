385a37ed0498f2d244e92ed8387a877e4de176b2
mazajaja <you@example.com>
2022-04-21 23:25:31 +0600

    3. Реализация профессий в коде [#6837]

--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -2,25 +2,25 @@ package ru.job4j.inheritance;
 
 public class Surgeon extends Doctor {
 
-    private boolean IsOperating;
+    private boolean isOperating;
 
     public Surgeon() {
     }
 
     public Surgeon(boolean isOperating) {
-        IsOperating = isOperating;
+       this.isOperating = isOperating;
     }
 
     public Surgeon(String name, String surname, String education, String birthday, boolean isCertified, boolean isOperating) {
         super(name, surname, education, birthday, isCertified);
-        IsOperating = isOperating;
+        this.isOperating = isOperating;
     }
 
     public boolean isOperating() {
-        return IsOperating;
+        return isOperating;
     }
 
     public void setOperating(boolean operating) {
-        IsOperating = operating;
+        isOperating = operating;
     }
 }
