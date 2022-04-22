9e62a9cc9d21215a716e84c2a56077b237c12b18
mazajaja <you@example.com>
2022-04-22 19:47:31 +0600

    6. Date. Отображение даты. [#310017 #292027]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.created.format(formatter);
+        String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
     }
 }
