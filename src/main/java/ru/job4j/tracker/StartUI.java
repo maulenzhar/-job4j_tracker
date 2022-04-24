38b0586c1f43abd084d8707ce1e4bd3fff7003cc
mazajaja <you@example.com>
2022-04-24 12:53:35 +0600

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        System.out.println(item);
     }
 }
