ae6da72f692c873f2130f8e7a307a93ced033acf
mazajaja <you@example.com>
2022-04-26 01:20:40 +0600

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -13,7 +13,6 @@ public class Matches {
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
             turn = !turn;
-            /* Остальная логика игры. */
             if ((matches >= 1 && matches <= 3)) {
                 count = count - matches;
                 System.out.println("Спичек осталось " + count);
