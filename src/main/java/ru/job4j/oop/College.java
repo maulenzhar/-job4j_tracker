3a7ca3a4b25eedd39a6cbddc4ac0440b846da160
mazajaja <you@example.com>
2022-04-28 01:40:54 +0600

    3. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 
 public class College {
     public static void main(String[] args) {
-        Student student = (Student) new Freshman();
-        Object obj = (Object) student;
+        Student student = new Freshman();
+        Object obj = student;
     }
 }
