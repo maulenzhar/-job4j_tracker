1f2c6c983534307e5e5980363087ab380beec5f6
mazajaja <you@example.com>
2022-04-30 01:35:37 +0600

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #294958]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             StartUI.showMenu();
-            int select = Integer.parseInt(input.askStr("Select: "));
+            int select = input.askInt("Select: ");
             if (select == 0) {
                 StartUI.createItem(input, tracker);
             } else if (select == 1) {
@@ -39,7 +39,7 @@ public class StartUI {
 
     public static void findItem(Input input, Tracker tracker) {
         System.out.println("=== Find item by id ===");
-        int id = Integer.parseInt(input.askStr("Enter id:  "));
+        int id = input.askInt("Enter id:  ");
         Item item = tracker.findById(id);
         if (item != null) {
             System.out.println(item);
@@ -50,7 +50,7 @@ public class StartUI {
 
     public static void deleteItem(Input input, Tracker tracker) {
         System.out.println("=== Delete item ===");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
+        int id = input.askInt("Enter id: ");
         if (tracker.delete(id)) {
             System.out.println("Заявка удалена успешно.");
         } else {
@@ -60,7 +60,7 @@ public class StartUI {
 
     public static void editItem(Input input, Tracker tracker) {
         System.out.println("=== Edit item ===");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
+        int id = input.askInt("Enter id: ");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
