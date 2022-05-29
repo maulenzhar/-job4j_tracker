d84e666eafc5da0241d1eed2e49c2ea6a72e16dc
mazajaja <you@example.com>
2022-05-30 01:10:50 +0600

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/collection/sorting/Job.java
+++ src/main/java/ru/job4j/collection/sorting/Job.java
@@ -19,10 +19,10 @@ public class Job implements Comparable<Job> {
 
     @Override
     public String toString() {
-        return "Job{" +
-                "name='" + name + '\'' +
-                ", priority=" + priority +
-                '}';
+        return "Job{"
+                + "name='" + name + '\''
+                + ", priority=" + priority
+                + '}';
     }
 
     @Override
