bc6b1d1222cc64e8a4a79805edb1e8fd20de140d
mazajaja <you@example.com>
2022-05-29 16:56:02 +0600

    2. Идентичные тексты [#212655 #306169]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String t : text) {
            if (!check.contains(t)) {
                rsl = false;
+               break;
            }
         }
         return rsl;
