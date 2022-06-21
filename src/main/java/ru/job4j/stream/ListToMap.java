338e752cb5395d30cb20ebbdb994f082bfd47523
mazajaja <you@example.com>
2022-06-21 15:23:07 +0600

    4. Преобразование List в Map. [#110226 #314756]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -2,7 +2,6 @@ package ru.job4j.stream;
 
 import ru.job4j.stream.school.Student;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
