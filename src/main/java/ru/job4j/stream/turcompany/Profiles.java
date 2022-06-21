015cee85eaebda3e5ebd6375ed1252639c3b645d
mzharylgasynov <$dbx254WSX>
2022-06-21 13:49:08 +0600

    2. Список адресов [#110062 #314446]

--- src/main/java/ru/job4j/stream/turcompany/Profiles.java
+++ src/main/java/ru/job4j/stream/turcompany/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream.turcompany;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +12,7 @@ public class Profiles {
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .sorted()
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
