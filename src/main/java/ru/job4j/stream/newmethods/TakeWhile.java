97d5607aad8051bc3824f65f12219121a06feeec
mazajaja <you@example.com>
2022-06-23 01:27:18 +0600

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/newmethods/TakeWhile.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream.newmethods;
+
+import java.util.List;
+
+public class TakeWhile {
+    public static void main(String[] args) {
+        List.of(1, 2, 3, 4).stream()
+                .takeWhile(v -> v < 3)
+                .map(v -> "Результат: " + v)
+                .forEach(System.out::println);
+
+        List.of(1, 2, 3, 4).stream()
+                .dropWhile(v -> v < 3)
+                .map(v -> "Результат: " + v)
+                .forEach(System.out::println);
+    }
+}
