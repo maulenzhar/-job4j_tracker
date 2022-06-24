fa543f309c7e481a093846edfdf178b52ccba3ef
mazajaja <you@example.com>
2022-06-24 12:14:04 +0600

    0.2. Перенос методов в Stream API [#279131 #315271]

--- src/main/java/ru/job4j/stream/card/Card.java
+++ src/main/java/ru/job4j/stream/card/Card.java
@@ -11,25 +11,29 @@ public class Card {
         this.suit = suit;
         this.value = value;
     }
-}
 
-class DoubleLoop {
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit=" + suit +
+                ", value=" + value +
+                '}';
+    }
+
     public static void main(String[] args) {
-        String[] levels = {"level 1", "level 2", "level 3"};
-        String[] tasks = {"task 1", "task 2", "task 3", "task 4"};
-        Stream.of(levels)
-                .flatMap(level -> Stream.of(tasks)
-                        .map(task -> level + " " + task))
+        Suit[] s = Suit.values();
+        Value[] v = Value.values();
+        Stream.of(s)
+                .flatMap(suit -> Stream.of(v)
+                        .map(value -> new Card(suit, value)))
                 .forEach(System.out::println);
     }
+
+    public enum Suit {
+        Diamonds, Hearts, Spades, Clubs
+    }
+
+    public enum Value {
+        V_6, V_7, V_8
+    }
 }
-
-enum Suit {
-    Diamonds, Hearts, Spades, Clubs
-}
-
-enum Value {
-    V_6, V_7, V_8
-}
-
-
