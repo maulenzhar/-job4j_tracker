386c0173e22cdf36ef42923e7d2771b1db6a3206
mzharylgasynov <$dbx254WSX>
2022-07-04 18:23:05 +0600

    2. Отсортировать департаменты [#34352 #318287]

--- src/main/java/ru/job4j/collection/departaments/Departments.java
+++ src/main/java/ru/job4j/collection/departaments/Departments.java
@@ -8,12 +8,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start.isEmpty()) {
-                    start = el;
-                    tmp.add(start);
-                    continue;
-                }
-                tmp.add(start + "/" + el);
+                start += "".equals(start) ? el : "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
