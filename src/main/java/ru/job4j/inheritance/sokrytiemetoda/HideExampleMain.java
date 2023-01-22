f6cb6fb683cee2b80e3c2bc318c16b6f703e7cb9
mzhar <life_thug@mail.ru>
2023-01-23 01:03:16 +0600

    other

--- src/main/java/ru/job4j/inheritance/sokrytiemetoda/HideExampleMain.java
+++ src/main/java/ru/job4j/inheritance/sokrytiemetoda/HideExampleMain.java
@@ -3,11 +3,11 @@ package ru.job4j.inheritance.sokrytiemetoda;
 /*Статический метод, определенный с той же сигнатурой в дочернем классе, называется скрытым. */
 public class HideExampleMain {
     public static void main(String[] args) {
-        Animal animal = new Animal(); // создаем объект типа Animal
-        Animal cat = new Cat(); // создаем объект Cat типа Animal
-        animal.instanceInvoke(); // вызов переопределенного метода класса Animal
-        cat.instanceInvoke(); // вызов переопределенного метода класса Cat
-        Animal.staticInvoke();  // вызов статического метода класса Animal
-        Cat.staticInvoke(); // вызов статического метода класса Cat
+        Animal animal = new Animal(); /* создаем объект типа Animal*/
+        Animal cat = new Cat(); /* создаем объект Cat типа Animal*/
+        animal.instanceInvoke(); /* вызов переопределенного метода класса Animal*/
+        cat.instanceInvoke(); /* вызов переопределенного метода класса Cat*/
+        Animal.staticInvoke();  /* вызов статического метода класса Animal*/
+        Cat.staticInvoke(); /* вызов статического метода класса Cat*/
     }
 }
