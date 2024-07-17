54924078312b3aa95f7e81b6eb510a7c200158f9
Maulen Zharylgasynov <exmpl@gmail.com>
2024-07-17 22:35:58 +0500

    Интеграционное тестирование с Hibernate

--- src/main/java/ru/job4j/lombok/Role.java
+++ src/main/java/ru/job4j/lombok/Role.java
@@ -5,8 +5,8 @@ import lombok.*;
 
 import java.util.List;
 
-@Builder(builderMethodName = "of") */
-/*Ключ builderMethodName задает имя метода создания объекта контейнера. По умолчанию он называется builder().*//*
+@Builder(builderMethodName = "of")
+Ключ builderMethodName задает имя метода создания объекта контейнера. По умолчанию он называется builder().
 
 @ToString
 @Getter
@@ -14,8 +14,8 @@ public class Role {
     private int id;
     private String name;
 
-    @Singular("accessBy")*/
-/*Аннотация @Singular позволяет аккумулировать отдельные элементы коллекции. Если ее убрать, то метод accessBy должен принимать список.*//*
+    @Singular("accessBy")
+Аннотация @Singular позволяет аккумулировать отдельные элементы коллекции. Если ее убрать, то метод accessBy должен принимать список.
 
     private List<String> accessBy;
 }
